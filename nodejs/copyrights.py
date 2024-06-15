dbddc803ad8624d2d622883cb9ca4f36af27fd1c
Rubier <166219464+Rubier-Project@users.noreply.github.com>
2024-06-15 20:53:20 +0330

    Delete nodejs/copyrights.py

--- nodejs/copyrights.py
+++
@@ -1,8 +0,0 @@
-import rubier.colors as colors
-import os
-
-os.system("")
-
-class ConsoleCopyrights(object):
-    def consolePrinter():
-        print(f"{colors.colors.MAGENTA}Rubier Library 3.9.4\n{colors.colors.CYAN}Rubino Framework\n\n{colors.colors.END}Started {colors.colors.ORANGE}Rubier{colors.colors.END} as Soon ...\n{colors.colors.LRED}-------------------------{colors.colors.END}\n\n")
