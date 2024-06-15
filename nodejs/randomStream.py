b2413cdd04db89548e20a3922dbd863cb0654867
Rubier <166219464+Rubier-Project@users.noreply.github.com>
2024-06-15 20:53:33 +0330

    Delete nodejs/randomStream.py

--- nodejs/randomStream.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class Stream(object):
-    def choiceStream(items: list = []):
-        return random.choice(items)
-    
-    def randomIntSteram() -> int:
-        return random.randint(100000, 999999999)
